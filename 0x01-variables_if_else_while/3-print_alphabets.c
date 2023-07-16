87527b2dadfe639e06889e4d06e920df41ded2cd
Curvyjay <osmanjuwairiah3gmail.com>
2023-07-16 14:03:15 -0700

    level

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-int main(){
-	char lowercase = 'a';
-	char uppercase  ='A';
-	// Print lowercase alphabet
-	while (lowercase <= 'z' ) {
-		putchar (lowercase);
-		lowercase++;
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
 	}
-	//Print uppercase alphabet
-	while (uppercase <= 'Z' ) {
-		putchar (uppercase);
-		uppercase++;
-	}
-	// Print new line
-	 putchar('\n');
-	// return 0;
+	putchar('\n');
+	return (0);
 }
+
+
+
