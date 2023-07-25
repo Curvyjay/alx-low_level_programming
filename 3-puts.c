d7b21f85d71e75ddac7717c35270ffdf6cac5a63
Curvyjay <osmanjuwairiah3gmail.com>
2023-07-25 16:03:03 -0700

    git

---
+++ 3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts-function that prints a string
+Char*str; string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i];
+	_putchar('\n');
+	return (0);
+	}
